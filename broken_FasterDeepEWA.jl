81a9f2cbacedd538757abf6878b671692fde2a54
M. van Dartel <145007942+mvdmvd@users.noreply.github.com>
2025-04-02 18:08:46 +0200

    .

--- broken_FasterDeepEWA.jl
+++ broken_FasterDeepEWA.jl
@@ -34,11 +34,10 @@ function init_EWA(;
     Q₀=[[0.0, 0.0], [0.0, 0.0]], # Prior attractions
     κ=0.0,
     α=1.0,
-    N₀=0.0,
     δ=1.0,
     payoff=pris_payoff,
     β=Inf64)
-    return Q₀, κ, α, N₀, δ, payoff, β # default params is best response dynamics
+    return Q₀, κ, α, δ, payoff, β # default params is best response dynamics
 end
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
@@ -73,12 +72,10 @@ end
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 function update_EWA(
     s::Vector{Int64},
-    N::Float64,
     Q::Vector{Vector{Float64}},
     κ::Float64, α::Float64, δ::Float64, payoff::Vector{Matrix{Int64}})
 
     Qₙ = [Float64[0.0, 0.0], Float64[0.0, 0.0]] # updated Q vector placeholder
-    Nₙ = (1 - α) * (1 - κ) * N + 1 # incremented history
 
     for i in 1:2 # for both players 
         j = 3 - i # j is opponent
@@ -86,91 +83,36 @@ function update_EWA(
         for a in 1:2 # update the attractions
             I = (a == sᵢ) ? 1 : 0 # indicator function for if action was played
             Π = payoff[i][a, s[j]] # find action's payoff
-            Qᵤ = ((1 - α) * N * Q[i][a] + (δ + (1 - δ) * I) * Π) / Nₙ # EWA updating rule
+            Qᵤ = ((1 - α) * Q[i][a] + (1 / (1 - (1 - α)(1 - κ))) * (δ + (1 - δ) * I) * Π) / Nₙ # EWA updating rule
             Qₙ[i][a] = Qᵤ # update attraction of each action
         end
     end
-    return Qₙ, Nₙ
+    return Qₙ
 end
 
 
-function EWA_step!(Q, N, κ, α, δ, payoff, β)
+function EWA_step!(Q, κ, α, δ, payoff, β)
     s, σ = mixed_strategy_draw(β, Q) # compute new mixed strat and draw action
-    Qₙ, Nₙ = update_EWA(s, N, Q, κ, α, δ, payoff) # compute new attractions based off payoffs
-    return Qₙ, Nₙ, s, σ
+    Qₙ = update_EWA(s, Q, κ, α, δ, payoff) # compute new attractions based off payoffs
+    return Qₙ, s, σ
 end
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # # Run simulation
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-function run_EWA_mixed(parameters; T=1000000000, ϵ=1e-4, print=false)
-    Q₀, κ, α, N₀, δ, payoff, β = parameters
-    print ? println("Prior Q: $Q₀, prior N: $N₀") : nothing
-
-    count_1 = 0
-    count_2 = 0
-
-    Qₜ, Nₜ, sₜ, σₜ = EWA_step!(Q₀, N₀, κ, α, δ, payoff, β) # first iter from priors
-
-    conv = 0
-    window = 0
-    NE_found = false
-    for t in 1:T
-        if ((t ≤ 10 && t % 2 == 0) || (t ≤ 100 && t % 50 == 0)) && print
-            print ? println("Iter $t: σ=$σₜ, s=$sₜ, Q=$Qₜ, N=$Nₜ") : continue
-        end
-
-        Qₜ, Nₜ, sₜ, σₜ = EWA_step!(Qₜ, Nₜ, κ, α, δ, payoff, β)
-
-        old_1 = count_1 / (t - 1)
-        old_2 = count_2 / (t - 1)
-
-        count_1 += sₜ[1] == 1 ? 1 : 0
-        count_2 += sₜ[2] == 1 ? 1 : 0
-
-        new_1 = count_1 / t
-        new_2 = count_2 / t
-
-        if abs(old_1 - new_1) < ϵ && abs(old_2 - new_2) < ϵ
-            window += 1
-        end
-
-        if t > 100 && window > 100
-            print ? println("Converged at iter: $t") : continue
-            break
-        end
-        conv = t
-    end
-    freq_1 = count_1 / conv
-    freq_2 = count_2 / conv
-    conv_sol = [[freq_1, 1 - freq_1], [freq_2, 1 - freq_2]]
-    NE = find_NE_mixed(payoff)
-
-    for ne in NE
-        for i in eachindex(ne)
-            for j in 1:2
-                if all(all.(abs.(conv_sol[j] .- ne[i]) .< 0.015))
-                    NE_found = true
-                end
-            end
-        end
-    end
-
-    return Qₜ, Nₜ, sₜ, σₜ, conv_sol, NE_found, NE
-end
 
 
 
 
 
 function run_EWA_pure(parameters; T=1000000, print=false)
-    Q₀, κ, α, N₀, δ, payoff, β = parameters
+    Q₀, κ, α, δ, payoff, β = parameters
     print ? println("Prior Q: $Q₀, prior N: $N₀") : nothing
 
     a₁ = 0
     a₂ = 0
 
-    Qₜ, Nₜ, sₜ, σₜ = EWA_step!(Q₀, N₀, κ, α, δ, payoff, β) # first iter from priors
+    Qₜ, sₜ, σₜ = EWA_step!(Q₀, κ, α, δ, payoff, β) # first iter from priors
 
     conv = 0
     window = 0
@@ -184,7 +126,7 @@ function run_EWA_pure(parameters; T=1000000, print=false)
         old_a₁ = a₁
         old_a₂ = a₂
 
-        Qₜ, Nₜ, sₜ, σₜ = EWA_step!(Qₜ, Nₜ, κ, α, δ, payoff, β)
+        Qₜ, sₜ, σₜ = EWA_step!(Qₜ, κ, α, δ, payoff, β)
 
         a₁ = sₜ[1]
         a₂ = sₜ[2]
@@ -203,7 +145,7 @@ function run_EWA_pure(parameters; T=1000000, print=false)
     Nash = find_NE_pure(payoff)
     NE_found = sₜ ∈ Nash ? true : NE_found
 
-    return Qₜ, Nₜ, sₜ, σₜ, lim_chaos, NE_found, Nash
+    return Qₜ, sₜ, σₜ, lim_chaos, NE_found, Nash
 end
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
