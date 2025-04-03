4b2370dda22a4ca78a3eae314dd057a661a18309
M. van Dartel <145007942+mvdmvd@users.noreply.github.com>
2025-04-03 14:47:03 +0200

    feat: dataEWA for generating data

--- FastEWA.jl
+++ FastEWA.jl
@@ -2,14 +2,15 @@ module fEWA
 
 using Distributions, GameTheory
 
-function find_NE_mixed(payoff)
-    g = NormalFormGame([Player(payoff[1]), Player(payoff[2])])
-    found = support_enumeration(g)
-    NE = [collect(ne) for ne in found]
+# returns a vector of all pure and mixed NE for a 2x2 normal form in the format Vector{Matrix{Int64}}
+function find_NE_mixed(payoff::Vector{Matrix{Int64}})
+    g = NormalFormGame([Player(payoff[1]), Player(payoff[2])]) # GameTheory.jl game formatting
+    found = support_enumeration(g) # solve for NE with GameTheory.jl
+    NE = [collect(ne) for ne in found] # convert to regular vector
     return NE
 end
 
-#=
+#= # This version finds only the pure NE, it's faster but utility depends on usecase
 function find_NE_pure(payoff)
     g = NormalFormGame([Player(payoff[1]), Player(payoff[2])])
     found = pure_nash(g)
@@ -21,66 +22,62 @@ end
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # # Initialisation
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-dom = [[5 0; 20 1], [5 0; 20 1]]
-dom_nash = fEWA.find_NE_mixed(dom)
-dom_game = [dom, dom_nash]
+dom = [[5 0; 20 1], [5 0; 20 1]] # standard domination game
+dom_game = [dom, fEWA.find_NE_mixed(dom)]
 
-function init_EWA(;
-    s₀=[0, 0],
-    μ₀=[0.0, 0.0],
-    Q₀=[[0.0, 0.0], [0.0, 0.0]], # Prior attractions
-    N₀=0.0,
-    α=1.0,
-    κ=0.0,
-    δ=1.0,
-    β=Inf64,
-    game=dom_game
-)
-    return s₀, μ₀, Q₀, N₀, α, κ, δ, β, game # default params is best response dynamics
+function init_EWA(; # initialisation function
+    s₀=[0, 0],                   # empty actions vector
+    μ₀=[0.0, 0.0],               # μᵢ represents the probability of playing a₁ based on the history of player i
+    Q₀=[[0.0, 0.0], [0.0, 0.0]], # prior attractions (technically a parameter)
+    N₀=0.0,                      # prior history (also a parameter)
+    α=1.0,                       # memory loss
+    κ=0.0,                       # discount rate
+    δ=1.0,                       # degree of foregone payoffs consideration
+    β=Inf64,                     # thermodynamic beta, controls stochasticity (β→lim ∞ = deterministic policy, β→lim ∞ = fully random policy)
+    game=dom_game)
+    return s₀, μ₀, Q₀, N₀, α, κ, δ, β, game # default parameterisation: best response dynamics for a domination game (pure NE always found). 
 end
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # # Simulation
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-function EWA_step!(
+function EWA_step!( # step function first selects actions based off attractions, then updates attractions based off actions
     sₜ::Vector{Int64}, μ::Vector{Float64}, Qₜ::Vector{Vector{Float64}}, Nₜ::Float64,
     α::Float64, κ::Float64, δ::Float64, β::Float64, payoff::Vector{Matrix{Int64}})
 
     # new actions
-    @inbounds for i ∈ 1:2 # for both players
-        Q₁, Q₂ = Qₜ[i...] # get attractions vector for player i
-        if isinf(β) # if deterministic policy:
+    @inbounds for i ∈ 1:2 # iterate over players
+        Q₁, Q₂ = Qₜ[i...] # get current attractions
+        if isinf(β) # check if deterministic policy, if so, do fast action selection
             if Q₁ > Q₂
                 sₜ[i] = 1
                 continue
             elseif Q₁ < Q₂
                 sₜ[i] = 2
                 continue
-            else # tie breaker, I guess this is still stochastic, maybe choose deterministically?
-                sₜ[i] = (rand(Bernoulli(0.5)) == 1 ? 1 : 2)
+            else # tie breaker is now 50/50 assignment (note: this is stochastic, however we follow the EWA equations)
+                sₜ[i] = (rand(Bernoulli(0.5)) == 1 ? 1 : 2) # draw action
             end
-        else # Numerically stable softmax
-            local s1 = β * Q₁
-            local s2 = β * Q₂
-            local m = s1 > s2 ? s1 : s2
-            local exp1 = exp(s1 - m)
-            local exp2 = exp(s2 - m)
-            local p₁ = exp1 / (exp1 + exp2)
+        else # stochastic policy
+            local s1, s2 = β * Q₁, β * Q₂
+            local m = s1 > s2 ? s1 : s2 # find max
+            local exp1, exp2 = exp(s1 - m), exp(s2 - m)
+            local p₁ = exp1 / (exp1 + exp2) # softmax, subtracting the max for numerical stability
             sₜ[i] = (rand(Bernoulli(p₁)) == 1 ? 1 : 2)
         end
-        μ[i] += sₜ[i] == 1 ? 1.0 : 0.0
+        μ[i] += sₜ[i] == 1 ? 1.0 : 0.0 # numerator for bayesian updating
     end
 
-    local Nₙ = (1 - α) * (1 - κ) * Nₜ + 1 # incremented history
-
-    @inbounds for i ∈ 1:2 # for both players 
-        j = 3 - i # j is opponent
-        for aᵢ ∈ 1:2 # update the attractions
-            I = (aᵢ == sₜ[i]) ? 1.0 : 0.0 # indicator function for if action was played
-            Πₐ = payoff[i][aᵢ, sₜ[j]] # find action's payoff
+    # update attractions
+    local Nₙ = (1 - α) * (1 - κ) * Nₜ + 1 # update history
+    @inbounds for i ∈ 1:2
+        j = 3 - i # j is other player
+        for aᵢ ∈ 1:2 # iterate over action space
+            I = (aᵢ == sₜ[i]) ? 1.0 : 0.0 # indicator function for action was played
+            Πₐ = payoff[i][aᵢ, sₜ[j]] # payoff for action
             local oldQ = Qₜ[i][aᵢ]
-            Qₜ[i][aᵢ] = ((1 - α) * Nₜ * oldQ + (δ + (1 - δ) * I) * Πₐ) / Nₙ # EWA updating rule
+            Qₜ[i][aᵢ] = ((1 - α) * Nₜ * oldQ + (δ + (1 - δ) * I) * Πₐ) / Nₙ # EWA updating
         end
     end
     return sₜ, μ, Qₜ, Nₜ
@@ -91,13 +88,13 @@ function Run_FastEWA(parameters::Tuple{Vector{Int64},Vector{Float64},Vector{Vect
     s₀, μ₀, Q₀, N₀, α, κ, δ, β, game = parameters
     payoff, NE = game
 
-    local NE_found = false
-    sₜ, μ, Qₜ, Nₜ = EWA_step!(s₀, μ₀, Q₀, N₀, α, κ, δ, β, payoff) # first iter from priors
-
-    @inbounds for t in 1:T
+    # convergence criterion is whether the expectation of the histories of play are an NE
+    local NE_found = false # set to true if converged = NE 
+    sₜ, μ, Qₜ, Nₜ = EWA_step!(s₀, μ₀, Q₀, N₀, α, κ, δ, β, payoff) # the first EWA step is based on the priors
+    @inbounds for t in 1:T # T=1000 is assumed to be close enough to ∞, test this for more rigor (low T is required for speed).
         sₜ, μ, Qₜ, Nₜ = EWA_step!(sₜ, μ, Qₜ, Nₜ, α, κ, δ, β, payoff)
-        local μ₁, μ₂ = μ ./ (t + 1)
-        local σ = [[μ₁, 1 - μ₁], [μ₂, 1 - μ₂]]
+        local μ₁, μ₂ = μ ./ (t + 1) # bayesian updating of a₁ probabilities 
+        local σ = [[μ₁, 1 - μ₁], [μ₂, 1 - μ₂]] # 
 
         if any(isapprox(σ, ne, atol=0.1) for ne in NE)
             NE_found = true
